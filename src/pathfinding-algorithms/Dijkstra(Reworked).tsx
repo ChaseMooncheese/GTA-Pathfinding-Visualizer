75cde937f81d85ae88b6c6fc9dad76a6d62fc893
Manny Modrono <mmodrono21243@columbushs.com>
2023-04-26 14:59:20 -0400

    Dijkstra's now works

--- src/pathfinding-algorithms/Dijkstra(Reworked).tsx
+++ src/pathfinding-algorithms/Dijkstra(Reworked).tsx
@@ -1,64 +1,72 @@
 import "../types/PathfindingVisualizerTypes";
 
-function Dijkstra(source: MapNode, destination: MapNode) {
-  let s = [source]; //visited nodes
-  let vs: MapNode[] = [];
-  source.edges.forEach((i) => vs.push(i[0]));
-  let d: number[] = [0];
-  let p: (number | MapNode)[] = [-1];
+export default function Dijkstra(source: MapNode, destination: MapNode) {
+  const s = [source]; //visited nodes
+  const vs: MapNode[] = []; //non visited nodes (non processed nodes)
+  source.edges.forEach((i) => vs.push(i[0])); //adds source's edges to vs to be processed
+  const d: number[] = [0]; //weights from source
+  const p: (-1 | MapNode)[] = [-1]; //parent node
 
-  let nodes = [source];
+  const nodes = [source]; //keeps track of all nodes seen so far
 
   for (let i = 0; i < vs.length; i++) {
+    //initializes d, nodes, and p with source edges
     d.push(source.edges[i][1]);
     nodes.push(vs[i]);
     p.push(source);
   }
 
   while (vs.length != 0) {
+    //runs while nodes still need to be processed
     let smallestNode = vs[0];
     let smallestNodeIndex = 0;
     vs.forEach((i) => {
-      if (
-        d[nodes.indexOf(smallestNode)] > d[nodes.indexOf(i)] &&
-        !s.includes(i)
-      ) {
+      //finds smallest(lowest weight from source) unprocessed node
+      if (d[smallestNodeIndex] > d[nodes.indexOf(i)] && !s.includes(i)) {
         smallestNode = i;
         smallestNodeIndex = nodes.indexOf(i);
       }
     });
     vs.splice(smallestNodeIndex, 1);
-    s.push(smallestNode);
-    smallestNode.edges.forEach((i) => {
-      if (!vs.includes(i[0]) && !s.includes(i[0])) {
-        vs.push(i[0]);
-      }
-      if (!nodes.includes(i[0])) {
-        nodes.push(i[0]);
-        d.push(Number.MAX_VALUE);
-      }
-    });
-    smallestNode.edges.forEach((i) => {
-      const index = nodes.indexOf(i[0]);
-      if (d[index] == undefined) {
-        d[index] = d[nodes.indexOf(smallestNode)] + i[1];
-        p[index] = smallestNode;
-      } else if (
-        vs.includes(i[0]) &&
-        d[nodes.indexOf(smallestNode)] + i[1] < d[index]
-      ) {
-        d[index] = d[nodes.indexOf(smallestNode)] + i[1];
-        p[index] = smallestNode;
-      }
-    });
+    s.push(smallestNode); //removes smalleset node from vs and adds it to s
+    if (smallestNode.edges !== undefined) {
+      //prevents errors for nodes with no edges
+      smallestNode.edges.forEach((i) => {
+        //adds new nodes
+        if (!vs.includes(i[0]) && !s.includes(i[0])) {
+          vs.push(i[0]);
+        }
+        if (!nodes.includes(i[0])) {
+          nodes.push(i[0]);
+          d.push(Number.MAX_VALUE);
+        }
+      });
+      smallestNode.edges.forEach((i) => {
+        const index = nodes.indexOf(i[0]);
+        if (d[index] == undefined) {
+          d[index] = d[nodes.indexOf(smallestNode)] + i[1];
+          p[index] = smallestNode;
+        } else if (
+          vs.includes(i[0]) &&
+          d[nodes.indexOf(smallestNode)] + i[1] < d[index]
+        ) {
+          d[index] = d[nodes.indexOf(smallestNode)] + i[1];
+          p[index] = smallestNode;
+        }
+      });
+    }
   }
 
   //path to destination
-  let path: (MapNode | number)[] = [destination];
-  let nodesTS: (MapNode | number)[] = nodes;
+  const path: MapNode[] = [destination];
+  const nodesTS: (MapNode | number)[] = nodes;
   while (path[0] != source) {
-    path.unshift(p[nodesTS.indexOf(path[0])]);
+    let pNode = p[nodesTS.indexOf(path[0])];
+    if (pNode === -1) {
+      pNode = source;
+    }
+    path.unshift(pNode);
   }
 
-  return path;
+  return [path, path];
 }
