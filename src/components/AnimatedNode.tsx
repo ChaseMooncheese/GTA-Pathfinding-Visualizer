32c0818fa36d4b913b1453f5a60b93a4177e2420
ChaseMooncheese <rchasemooney@gmail.com>
2023-04-27 01:18:42 -0400

    quick commit to test brian

---
+++ src/components/AnimatedNode.tsx
@@ -0,0 +1,37 @@
+import { LatLng } from "leaflet";
+import { useEffect, useState } from "react";
+import { Circle } from "react-leaflet";
+
+const radius = 3;
+const weight = 1;
+
+export default function AnimatedNode(props: {
+  delay: number;
+  position: LatLng;
+  nodeType: "shortestPath" | "visited";
+  animationPending: boolean;
+}) {
+  const [currentRadius, setCurrentRadius] = useState(3);
+  const [isVisible, setIsVisible] = useState(false);
+  const color = props.nodeType === "shortestPath" ? "yellow" : "red";
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, props.delay);
+    return () => clearTimeout(timer);
+  }, []);
+
+  console.log(isVisible);
+
+  return isVisible ? (
+    <Circle
+      center={props.position}
+      radius={currentRadius}
+      color={color}
+      weight={1}
+    />
+  ) : (
+    <></>
+  );
+}
